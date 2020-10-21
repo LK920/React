0fb7779d429102d9ee5901e9044587ec84ca2999
LK920 <hopeone1214@gmail.com>
2020-10-21 12:10:13 +0900

    ch08 컴포넌트 훅

---
+++ ch08/src/components/UseRefCompo.js
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+
+const UseRefCompo = () => {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+
+  //ref 생성
+  const inputRef = useRef();
+
+  const onClicked = () => {
+    const nextList = list.concat(inputRef.current.value);
+    setList(nextList);
+    setValue("");
+  };
+  const onChanged = (e) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div className="UseRefCompo">
+      <h3>UseRefCompo</h3>
+
+      <ul>
+        {list.map((value, index) => {
+          return <li key={index}>{value}</li>;
+        })}
+      </ul>
+      <input type="text" ref={inputRef} value={value} onChange={onChanged} />
+      <button onClick={onClicked}>등록</button>
+    </div>
+  );
+};
+
+export default UseRefCompo;
